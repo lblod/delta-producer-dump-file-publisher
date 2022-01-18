6544000a6e985cd8fe57dffb67d56ccaec0292d8
felix <ruiz.felixruiz@gmail.com>
2022-01-18 10:51:15 +0100

    Rename variable

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/delta', async function( req, res ) {
 async function produceDumpFile(config, task) {
   try {
     console.log(`Generating dump file for task ${task}.`);
-    const manager = new DatasetManager(config.dcatDataSetSubject, config.targetGraph, config.fileBaseName, config.publicationEndpoint);
+    const manager = new DatasetManager(config.dcatDataSetSubject, config.targetGraph, config.fileBaseName, config.publicationGraphEndpoint);
     await updateStatus(task, STATUS_BUSY);
     await manager.createDumpFile();
     await updateStatus(task, STATUS_SUCCESS);
